253286a992f040d815e347938763ae95e8c4b57d
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-05-06 09:44:01 +0200

    Load data from progress.csv and use this for data

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
+const httpRequest = require('./httpRequest.js')
+
 window.onload = () => {
-  render()
+  httpRequest('progress.csv', {}, (err, result) => {
+    if (err) {
+      return alert(err)
+    }
+
+    let rows = result.body.split(/\n/g)
+    let labels = []
+    let data = []
+
+    rows.forEach(row => {
+      let v = row.split(/:/)
+      labels.push(v[0])
+      data.push(v[1])
+    })
+
+    render(labels, data)
+  })
 }
 
-function render () {
+function render (labels, data) {
   const ctx = document.getElementById('chart').getContext('2d');
   const myChart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: ['a', 'b', 'c', 'd'],
+      labels,
       datasets: [{
 	label: 'Unterschriften',
-	data: [1, 2, 6, 7],
+	data,
 	backgroundColor: '#f00',
 	borderColor: '#000'
       }]
